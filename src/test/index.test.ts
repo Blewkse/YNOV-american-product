4a91603177eaabfdc682aaa9f736a1187ca2ed94
Blewkse <julesbloux@gmail.com>
2025-02-27 11:46:48 +0100

    fix error test

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,19 +1,20 @@
 import { describe, expect, it } from "bun:test";
+import { getCartPrice } from "..";
 
 describe("Price by cart", () => {
   it("test 1 product", () => {
     expect(getCartPrice(1, 2.5)).toBe(2.5);
   });
-  it("test 1 product", () => {
+  it("test 0 product", () => {
     expect(getCartPrice(0, 2.5)).toBe(0);
   });
-  it("test 1 product", () => {
-    expect(getCartPrice(-1, 2.5)).toThrowError();
+  it("test -1 product", () => {
+    expect(() => getCartPrice(-1, 2.5)).toThrow("Invalid input");
   });
-  it("test 1 product", () => {
+  it("test 4 products", () => {
     expect(getCartPrice(4, 2.5)).toBe(10);
   });
-  it("test 1 product", () => {
+  it("test 4 products other price", () => {
     expect(getCartPrice(4, 3.5)).toBe(14);
   });
 });
